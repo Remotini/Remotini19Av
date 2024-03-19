7a3809b9a1f8fdaf5786150b155b6c78f4b8a55b
iyed8 <iyedacademy6@gmail.com>
2024-03-20 00:31:17 +0100

    update-Header
    Notification_box

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoIosNotificationsOutline } from "react-icons/io";
 function Header() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [mode, setMode] = useState("inactive");
+  const handleClick = () => {
+    setMode(mode === "active" ? "inactive" : "active");
+    console.log(mode);
+  };
   return (
     <div className=" flexCenter h-wrapper">
       <span className="logo">
@@ -10,8 +16,20 @@ function Header() {
         <span className=" Logo">emotini</span>
       </span>
       <div className=" icon-container">
-        <IoIosNotificationsOutline />
+        <IoIosNotificationsOutline
+          // on the onclick make a function that will open the notification page
+          onClick={() => {
+            handleClick();
+          }}
+        />
         <IoSettingsOutline />
+        <div className="notification-box">
+          {isVisible && (
+            <div className={`notification ${mode}`}>
+              <p>put the notification here aazeazehatrrtrtzuieauieg</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
